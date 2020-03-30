bf9aa896d54732f54ffc203b37dd36d2db6e799c
Omar Muhanna <omarmuhomar@hotmail.com>
2020-03-30 14:39:22 -0500

    added highlight, and other comp...

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import Header from './Header/Header.js'
 import Footer from './Footer/Footer.js'
+import Intro from './Intro/Intro.js'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Intro />
       <Footer />
      
     </div>
