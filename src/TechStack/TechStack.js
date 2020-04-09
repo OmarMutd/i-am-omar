3c7b9c1a856694ff86d4fd6a8180295d9e1103b1
Omar Muhanna <omarmuhomar@hotmail.com>
2020-04-09 18:57:00 -0500

    updated techstack page

--- src/TechStack/TechStack.js
+++ src/TechStack/TechStack.js
@@ -13,9 +13,28 @@ function TechStack() {
   <Navbar />
       <div className='section-wrapper'>
         {/* <p>I spend my time building new websites and tools while simultaneously willing quirky side-projects into existence. I also have an unhealthy obsession with Elon Musk, I tend to impulsively learn new things for no apparent reason, and I think Indie music is pretty cool.</p> */}
-        <p className='underline'><br></br>Tech.json:</p>
+        <p className='underline'><br></br>Frontend.json:</p>
         {/* <WordList words={['HTML5', 'CSS3', 'Javascript', 'jQuery', 'React', 'Git', 'AJAX', 'Node', 'Postgres', 'Python']} /> */}
         <div className='my-code'>
+          <Highlight className="json">
+            {`{
+  "The cool stuff on the screen": [
+    "HTML5",
+    "CSS3",
+    "Javascript",
+    "jQuery",
+    "React",
+    "Git",
+    "AJAX",
+    "Node",
+    "Postgres",
+    `}
+            "<span className="blinking-cursor">|</span>"
+  {`
+  ]
+}` }
+          </Highlight>
+          <p className='underline'><br></br>Backend.json:</p>
           <Highlight className="json">
             {`{
   "Tech Stack": [
@@ -35,7 +54,18 @@ function TechStack() {
 }` }
           </Highlight>
         </div>
-        <p>I also speak English and Arabic.</p>
+        <p className='underline'><br></br>Languages.json:</p>
+        <Highlight className="json">
+            {`{
+  "Sounds that I can make": [
+    'English',
+    'Arabic',
+    `}
+            "<span className="blinking-cursor">|</span>"
+  {`
+  ]
+}` }
+          </Highlight>
       </div>
     </section>
   );
